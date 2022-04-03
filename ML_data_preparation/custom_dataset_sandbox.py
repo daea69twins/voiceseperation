792b666f3b7dd09b8494cbc80a33c414ccb407dd
Alex-R-Brown <alexander-r-brown@uiowa.edu>
2022-04-03 11:39:29 -0500

    Preparing the supporting files for training on CoLab

--- ML_data_preparation/custom_dataset_sandbox.py
+++ ML_data_preparation/custom_dataset_sandbox.py
@@ -5,7 +5,7 @@
 
 
 """
-The goal is to take the original code from the asteroid library for an inherited, custom Dataset object 
+The goal is to take the original code from the asteroid library for an inherited, custom Dataset object
 and fit it to our needs.
 
 Original code:
@@ -39,8 +39,8 @@ class CustomMixture(Dataset):
         Efthymios Tzinis and Jens Heitkaemper and Michel Olvera and Fabian-Robert Stöter and
         Mathieu Hu and Juan M. Martín-Doñas and David Ditter and Ariel Frank and Antoine Deleforge
         and Emmanuel Vincent. 2020
-        
-        
+
+
         @inproceedings{Pariente2020Asteroid,
         title={Asteroid: the {PyTorch}-based audio source separation toolkit for researchers},
         author={Manuel Pariente and Samuele Cornell and Joris Cosentino and Sunit Sivasankaran and
@@ -54,7 +54,7 @@ class CustomMixture(Dataset):
 
 
     def __init__(
-        self, csv_dir, dataset_name, sample_rate=22040, 
+        self, csv_dir, dataset_name, sample_rate=22040,
     ):
         self.csv_dir = csv_dir
 
@@ -63,39 +63,39 @@ class CustomMixture(Dataset):
         self.csv_path = os.path.join(self.csv_dir, md_file)
 
         self.sample_rate = sample_rate
-        
+
         # Open csv file
         self.df = pd.read_csv(self.csv_path)
-        
+
 
     def __len__(self):
         return len(self.df)
-    
+
 
     def __getitem__(self, idx):
         print("IN __getitem__()")
 
         # Get the row in dataframe
         row = self.df.iloc[idx]
-        
+
         # Get mixture path
         mixture_path = row["mixture_path"]
         self.mixture_path = mixture_path
         sources_list = []
-        
+
         start = 0
-        stop = None  
+        stop = None
 
         # Read sources
-        source_path = row["source_char_path"]
+        source_path = row["clean_path"]
         s, _ = sf.read(source_path, dtype="float32", start=start, stop=stop)
         sources_list.append(s)
-        source_path = row["source_other_path"]
+        source_path = row["noise_path"]
         s, _ = sf.read(source_path, dtype="float32", start=start, stop=stop)
         sources_list.append(s)
 
         print("READ METADATA COMPLETE")
-            
+
         # Read the mixture
         mixture, _ = sf.read(mixture_path, dtype="float32", start=start, stop=stop)
 
@@ -104,7 +104,7 @@ class CustomMixture(Dataset):
         # Convert to torch tensor
         print("mixture.shape:", mixture.shape)
         mixture = torch.from_numpy(mixture)
-        
+
         # Stack sources (this puts the sources in the same array, but does not combine them)
         sources = np.vstack(sources_list)
         print("sources.shape:", sources.shape)
@@ -116,9 +116,8 @@ class CustomMixture(Dataset):
         # sources = np.swapaxes(sources, 0, 1)
         # mixture = np.swapaxes(mixture, 0, 1)
 
-        
-        
 
-        
+
+
+
         return mixture, sources
-
